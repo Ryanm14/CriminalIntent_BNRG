cdef4d86d9f79e27a924e5f25a53382d969f3161
ryanm14 <ryanm1114@gmail.com>
2015-11-28 16:02:34 -0500

    new layout

--- app/src/main/java/me/ryanmiles/criminalintent/model/Crime.java
+++ app/src/main/java/me/ryanmiles/criminalintent/model/Crime.java
@@ -12,6 +12,7 @@ public class Crime {
     private String mTitle;
     private Date mDate;
     private boolean mSolved;
+    private String mSuspect;
 
     public Crime(){
         mId = UUID.randomUUID();
@@ -53,5 +54,13 @@ public class Crime {
 
     public void setSolved(boolean solved) {
         mSolved = solved;
+    }
+
+    public String getSuspect() {
+        return mSuspect;
+    }
+
+    public void setSuspect(String mSuspect) {
+        this.mSuspect = mSuspect;
     }
 }
